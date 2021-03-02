9a02ad5202ad58bac5cb10f771b46abe6361da72
aliravn <alira.vn@gmail.com>
2021-03-02 23:23:23 +0100

    working webpack setup from scratch

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() { 
+        return (
+            <div>
+                <h1>AOC2019 with React. It's Alive!!!</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
